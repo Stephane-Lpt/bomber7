5f0bde8ac554a81a7b6529c268d055e3258e1bf4
nikita <nikita.ziuzin@dada.fr>
2025-05-25 23:06:29 +0200

    feat: main menu implementation start

--- lwjgl3/src/main/java/com/bomber7/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/bomber7/lwjgl3/Lwjgl3Launcher.java
@@ -1,8 +1,9 @@
 package com.bomber7.lwjgl3;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.bomber7.core.Bomber7;
+import com.bomber7.core.BomberGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,15 +13,19 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Bomber7(), getDefaultConfiguration());
+        return new Lwjgl3Application(new BomberGame(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        Graphics.DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+
         configuration.setTitle("bomber7");
+
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
+
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
@@ -28,10 +33,10 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setWindowedMode(640, 480);
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+//        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(displayMode.width * 2/3, displayMode.height * 2/3);
+//        configuration.setWindowIcon();
+
         return configuration;
     }
 }
