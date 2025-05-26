d6adf9a6265d6319adc3b710dc00f54051d720e9
t70n <97734858+t70n@users.noreply.github.com>
2025-05-26 10:51:52 +0200

    [Not_Finished] : Character and Player Class with Exceptions, without Test-cases

---
+++ core/src/main/java/n7/bomber7/model/exception/IllegalBombOperationException.java
@@ -0,0 +1,13 @@
+package n7.bomber7.model.exception;
+
+public class IllegalBombOperationException extends RuntimeException {
+    
+    /**
+	 * IllegalBombOperationException Exception constructor
+	 * @param message
+	 */
+    public IllegalBombOperationException(String message) {
+        super(message);
+    }
+
+}
