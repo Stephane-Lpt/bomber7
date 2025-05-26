f343a7f743f94678515d1457d668d19a42648fe2
p4cm4n <thomassilvestre31@gmail.com>
2025-05-26 16:11:21 +0200

    init map

--- core/src/main/java/com/bomber7/core/model/LevelMap.java
+++ core/src/main/java/com/bomber7/core/model/LevelMap.java
@@ -1,5 +1,9 @@
 package com.bomber7.core.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Represents a level with a map in the game.
  * Responsible for loading and parsing the map data from a file.
@@ -9,7 +13,11 @@ public class LevelMap {
     /**
      * The file name or path from which the map is loaded.
      */
+    private static final int WIDTH = 35;
+    private static final int HEIGHT = 24;
+
     private String fileName;
+    private Square[][] map;
 
     /**
      * Constructs a new LevelMap by loading it from the specified file.
@@ -18,6 +26,7 @@ public class LevelMap {
      */
     LevelMap(String fileName) {
         this.fileName = fileName;
+        this.map = new Square[HEIGHT][WIDTH];
         loadMap(fileName);
     }
 
@@ -28,7 +37,28 @@ public class LevelMap {
      * @param filepath  the path to the map file
      */
     private void loadMap(String filepath) {
-        // Load map from file
-        // Parse the file and create the map
+        try (BufferedReader br = new BufferedReader(new FileReader(this.fileName))) {
+            String ligne;
+            int numeroLigne = 0;
+
+            while ((ligne = br.readLine()) != null) {
+                // Découpe la ligne en colonnes selon la virgule
+                String[] colonnes = ligne.split(",");
+
+
+                for (int i = 0; i < colonnes.length; i++) {
+                    Square s = new Square(numeroLigne, i);
+                    this.map[numeroLigne][i] = s;
+                    
+                }
+
+                numeroLigne++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
