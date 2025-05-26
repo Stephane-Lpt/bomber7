f47ce16f5283855c4c2208476cf4a9c86840365c
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-05-26 20:12:07 +0200

    Adding junit dependency in gradle

---
+++ core/src/test/java/com/bomber7/core/TestHellos.java
@@ -0,0 +1,25 @@
+package com.bomber7.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHellos {
+
+    @Test
+    void testHello() {
+        String msg = "Hello";
+        assertEquals("Hello", msg);
+    }
+
+    @Test
+    void testHello2() {
+        String msg = "Hello";
+        assertEquals("Hello2", msg);
+    }
+
+    @Test
+    void testHello3() {
+        String msg = "Hello";
+        assertEquals("Hello", msg);
+    }
+}
