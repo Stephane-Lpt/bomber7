4eff5f638ea4b3512f3a3f42ead20127da35cb62
Yoann FRANCOIS <121094049+yfrancois-iut@users.noreply.github.com>
2025-05-27 21:49:08 +0200

    Beginning of MapParser

--- core/src/main/java/com/bomber7/core/model/LevelMap.java
+++ core/src/main/java/com/bomber7/core/model/LevelMap.java
@@ -1,64 +1,64 @@
-package com.bomber7.core.model;
+// package com.bomber7.core.model;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+// import java.io.BufferedReader;
+// import java.io.FileReader;
+// import java.io.IOException;
 
-/**
- * Represents a level with a map in the game.
- * Responsible for loading and parsing the map data from a file.
- */
-public class LevelMap {
+// /**
+//  * Represents a level with a map in the game.
+//  * Responsible for loading and parsing the map data from a file.
+//  */
+// public class LevelMap {
 
-    /**
-     * The file name or path from which the map is loaded.
-     */
-    private static final int WIDTH = 35;
-    private static final int HEIGHT = 24;
+//     /**
+//      * The file name or path from which the map is loaded.
+//      */
+//     private static final int WIDTH = 35;
+//     private static final int HEIGHT = 24;
 
-    private String fileName;
-    private Square[][] map;
+//     private String fileName;
+//     private Square[][] map;
 
-    /**
-     * Constructs a new LevelMap by loading it from the specified file.
-     *
-     * @param fileName  the name or path of the file to load the map from
-     */
-    LevelMap(String fileName) {
-        this.fileName = fileName;
-        this.map = new Square[HEIGHT][WIDTH];
-        loadMap(fileName);
-    }
+//     /**
+//      * Constructs a new LevelMap by loading it from the specified file.
+//      *
+//      * @param fileName  the name or path of the file to load the map from
+//      */
+//     LevelMap(String fileName) {
+//         this.fileName = fileName;
+//         this.map = new Square[HEIGHT][WIDTH];
+//         loadMap(fileName);
+//     }
 
-    /**
-     * Loads the map data from the given file path.
-     * The method is responsible for parsing the file and initializing the map structure.
-     *
-     * @param filepath  the path to the map file
-     */
-    private void loadMap(String filepath) {
-        try (BufferedReader br = new BufferedReader(new FileReader(this.fileName))) {
-            String ligne;
-            int numeroLigne = 0;
+//     /**
+//      * Loads the map data from the given file path.
+//      * The method is responsible for parsing the file and initializing the map structure.
+//      *
+//      * @param filepath  the path to the map file
+//      */
+//     private void loadMap(String filepath) {
+//         try (BufferedReader br = new BufferedReader(new FileReader(this.fileName))) {
+//             String ligne;
+//             int numeroLigne = 0;
 
-            while ((ligne = br.readLine()) != null) {
-                // Découpe la ligne en colonnes selon la virgule
-                String[] colonnes = ligne.split(",");
+//             while ((ligne = br.readLine()) != null) {
+//                 // Découpe la ligne en colonnes selon la virgule
+//                 String[] colonnes = ligne.split(",");
 
 
-                for (int i = 0; i < colonnes.length; i++) {
-                    Square s = new Square(numeroLigne, i);
-                    this.map[numeroLigne][i] = s;
+//                 for (int i = 0; i < colonnes.length; i++) {
+//                     Square s = new Square(numeroLigne, i);
+//                     this.map[numeroLigne][i] = s;
                     
-                }
+//                 }
 
-                numeroLigne++;
-            }
+//                 numeroLigne++;
+//             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
 
---
+++ src/model/Subject.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public interface Subject {
+
+    List<Observer> getObservers();
+
+    default void registerObserver(Observer observer){
+        getObservers().add(observer);
+    }
+    
+    default void deleteObserver(Observer observer){
+        getObservers().remove(observer);
+    }
+    
+    default void notifyObservers(){
+        for (Observer observer : getObservers()) {
+            observer.refresh(Subject.this);
+        }
+    }
+}
