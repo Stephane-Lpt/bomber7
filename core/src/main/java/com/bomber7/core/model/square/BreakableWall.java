8145a615066906929d2269238b0e8bd35769c5ac
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-05-28 08:42:18 +0200

    Adding squares related classes

---
+++ core/src/main/java/com/bomber7/core/model/square/BreakableWall.java
@@ -0,0 +1,18 @@
+package com.bomber7.core.model.square;
+
+/**
+ * Represents a breakable wall in the game.
+ */
+public class BreakableWall extends Wall {
+
+    /**
+     * Constructs a BreakableWall with the specified sprite file path and coordinates.
+     *
+     * @param spriteFilePath the file path to the sprite image for the wall
+     * @param xCoord         the x-coordinate of the wall on the game grid
+     * @param yCoord         the y-coordinate of the wall on the game grid
+     */
+    public BreakableWall(String spriteFilePath, int xCoord, int yCoord) {
+        super(spriteFilePath, xCoord, yCoord);
+    }
+}
