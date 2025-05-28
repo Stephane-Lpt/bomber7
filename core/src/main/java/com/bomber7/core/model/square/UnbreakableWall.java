2d10a6660722daa0beafe77234571c5d40851fc8
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-05-28 14:08:57 +0200

    Adding toString methods

--- core/src/main/java/com/bomber7/core/model/square/UnbreakableWall.java
+++ core/src/main/java/com/bomber7/core/model/square/UnbreakableWall.java
@@ -15,4 +15,9 @@ public class UnbreakableWall extends Wall {
     public UnbreakableWall(String spriteFilePath, int xCoord, int yCoord) {
         super(spriteFilePath, xCoord, yCoord);
     }
+
+    @Override
+    public String toString() {
+        return "UnbreakableWall";
+    }
 }
