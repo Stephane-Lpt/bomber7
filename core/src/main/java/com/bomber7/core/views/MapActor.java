9377e7a8aee509763901bf2ba577c3c2c7d4f64b
t70n <97734858+t70n@users.noreply.github.com>
2025-06-02 08:47:17 +0200

    Fix before review by the group

--- core/src/main/java/com/bomber7/core/views/MapActor.java
+++ core/src/main/java/com/bomber7/core/views/MapActor.java
@@ -8,12 +8,22 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
  * draw() is called by libGDX for each frame.
  */
 public class MapActor extends Actor {
+
+    /** The ViewMap instance : map grid + resources */
     private ViewMap viewMap;
 
+    /**
+     * Constructs a new MapActor with the specified ViewMap.
+     * @param viewMap the ViewMap 
+     */
     public MapActor(ViewMap viewMap) {
         this.viewMap = viewMap;
     }
 
+    /**
+     * Method called by libGDX to draw the map.
+     * It updates the map textures by iterating through each square in the grid.
+     */
     @Override
     public void draw(Batch batch, float parentAlpha) {
         viewMap.updateMapTextures();
