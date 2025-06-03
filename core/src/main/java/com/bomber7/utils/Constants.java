963f9222d1dd5594092344a5926f19000e5e6433
nikita <nikita.ziuzin@dada.fr>
2025-06-03 17:48:52 +0200

    fix: map now appears at the center of the screen and fits it

--- core/src/main/java/com/bomber7/utils/Constants.java
+++ core/src/main/java/com/bomber7/utils/Constants.java
@@ -4,4 +4,8 @@ package com.bomber7.utils;
  * Constant variables used in the app.
  */
 public final class Constants {
+    /**
+     * The size in pixels of map textures.
+     */
+    public final static int TEXTURE_SIZE = 32;
 }
