4375665ea43a92f02d92ecbeefc38aa199acd4d0
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-06-04 17:35:16 +0200

    checkstyle

--- core/src/test/java/com/bomber7/core/PlayerTest.java
+++ core/src/test/java/com/bomber7/core/PlayerTest.java
@@ -3,44 +3,52 @@ package com.bomber7.core;
 import com.bomber7.core.model.entities.Player;
 import com.bomber7.core.model.map.LevelMap;
 import com.bomber7.core.model.map.LevelMapFactory;
-import com.bomber7.core.model.square.*;
+import com.bomber7.core.model.square.Bomb;
+import com.bomber7.core.model.square.BombType;
+import com.bomber7.core.model.square.Square;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    /**
+     * Instance of Player used for testing (reset before each test).
+     */
     private Player player;
-    private LevelMap levelMap;
+
+    /**
+     * Instance of LevelMap (map = foy) used for testing (reset before each test).
+     */
+    private LevelMap foyLevelMap;
 
     @BeforeEach
     void setUp() {
-        // Dummy map: 5x5 with empty squares
-        this.levelMap = LevelMapFactory.createLevelMap("foy");
+        this.foyLevelMap = LevelMapFactory.createLevelMap("foy");
 
         // Create a concrete subclass of Player for testing
-        player = new Player("TestPlayer", this.levelMap, 1, 1, 3, 1, "sprite.png") {
+        player = new Player("TestPlayer", this.foyLevelMap, 1, 1, 3, 1, "sprite.png") {
         };
         player.setTypeBomb(BombType.TIME);
         player.setNbBomb(2);
     }
 
     @Test
-    void dropBomb_shouldSucceed_whenNbBombIsGreaterThan1() {
+    void dropBombWhenNbBombIsGreaterThan1OK() {
         boolean result = player.dropBomb();
         assertTrue(result, "Expected dropBomb to return true when nbBomb > 1");
 
-        Square square = this.levelMap.getSquare(1, 1);
+        Square square = this.foyLevelMap.getSquare(1, 1);
         assertTrue(square.getMapElement() instanceof Bomb, "Expected a Bomb to be placed on the square");
     }
 
     @Test
-    void dropBomb_shouldFail_whenNbBombIsLessThanOrEqualTo1() {
+    void dropBombWhenNbBombIsLessThanOrEqualTo1FAIL() {
         player.setNbBomb(1); // should fail
         boolean result = player.dropBomb();
         assertFalse(result, "Expected dropBomb to return false when nbBomb <= 1");
 
-        Square square = this.levelMap.getSquare(1, 1);
+        Square square = this.foyLevelMap.getSquare(1, 1);
         assertNull(square.getMapElement(), "Expected no Bomb to be placed when dropBomb fails");
     }
 }
