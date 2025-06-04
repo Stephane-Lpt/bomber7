8b56109d6c6ed4e2ffae22d99df1e5b0ddd8769c
nikita <nikita.ziuzin@dada.fr>
2025-06-04 08:29:11 +0200

    on going

--- core/src/main/java/com/bomber7/core/screens/KeyBindingScreen.java
+++ core/src/main/java/com/bomber7/core/screens/KeyBindingScreen.java
@@ -1,12 +1,16 @@
 package com.bomber7.core.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.bomber7.utils.Controls;
 
 /**
  * A screen where a player can bind its input controls.
  */
 public class KeyBindingScreen extends BomberScreen {
+    private TextButton[] inputButton
+
     /**
      * Constructs a new KeyBindingScreen associated with the given game.
      *
@@ -20,7 +24,8 @@ public class KeyBindingScreen extends BomberScreen {
     public void initView() {
         Table table = new Table();
 
-
+        Label[] inputLabels = new Label[Controls.values().length];
+        inputButtons = new TextButton[Controls.values().length];
     }
 
     @Override
---
+++ core/src/main/java/com/bomber7/utils/Controls.java
@@ -0,0 +1,31 @@
+package com.bomber7.utils;
+
+/**
+ * An enum that contains all the possible controls.
+ */
+public enum Controls {
+    /**
+     * Move the character up.
+     */
+    MOVE_UP,
+    /**
+     * Move the character down.
+     */
+    MOVE_DOWN,
+    /**
+     * Move the character left.
+     */
+    MOVE_LEFT,
+    /**
+     * Move the character right.
+     */
+    MOVE_RIGHT,
+    /**
+     * Plant a bomb.
+     */
+    PLANT_BOMB,
+    /**
+     * Activate a trigger bomb.
+     */
+    ACTIVATE_BOMB;
+}
