560656b17e19adf2c63e890812d347b30cb217d1
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-06-16 20:40:39 +0200

    Adding auto placing bomb and move to avoid explosion

--- core/src/main/java/com/bomber7/core/model/entities/AIPlayer.java
+++ core/src/main/java/com/bomber7/core/model/entities/AIPlayer.java
@@ -1,7 +1,13 @@
 package com.bomber7.core.model.entities;
 
 import com.bomber7.core.model.map.LevelMap;
+import com.bomber7.core.model.square.Square;
 import com.bomber7.utils.GameCharacter;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 /**
  * AI player class.
@@ -9,10 +15,25 @@ import com.bomber7.utils.GameCharacter;
 public class AIPlayer extends Player {
 
     /**
+     * Relative coordinates representing potential safe directions:
+     * Each array is composed of 4 values:
+     * [first step dx, first step dy, second step dx, second step dy]
+     * Meaning: if (first step) is walkable AND (second step) is walkable → it's a safe spot.
+     */
+    private static final int[][] SAFE_DIRECTIONS = {
+        { 1, 0,  1,  1}, // Right then Up-Right
+        { 1, 0,  1, -1}, // Right then Down-Right
+        {-1, 0, -1,  1}, // Left then Up-Left
+        {-1, 0, -1, -1}, // Left then Down-Left
+        { 0, 1,  1,  1}, // Up then Up-Right
+        { 0, 1, -1,  1}, // Up then Up-Left
+        { 0,-1,  1, -1}, // Down then Down-Right
+        { 0,-1, -1, -1}  // Down then Down-Left
+    };
+
+    /**
      * AIPlayer Constructor.
-     * @param config        The configuration for the player controls
      * @param map           The map.
-     * @param name          The name of the player
      * @param mapX          The x-coordinate of the player
      * @param mapY          The y-coordinate of the player
      * @param gameCharacter The game character type
@@ -26,7 +47,6 @@ public class AIPlayer extends Player {
      * The AI player is always moving towards the nearest player.
      * If no player is found, it will move randomly.
      * @param players The list of players in the game.
-     * @param map The current level map.
      */
     public void moveAI(Player[] players) {
         if (players == null || players.length == 0) {
@@ -39,6 +59,90 @@ public class AIPlayer extends Player {
         } else {
             moveRandomly();
         }
+    }
+
+    public void tick(float delta){
+
+
+        if(this.hasSafeSpot()) {
+            System.out.println("HAS SAFE SPOT");
+            if (this.dropBomb()) {
+                boolean hasMovedToSafeSpot = this.moveToSafeSpot();
+                System.out.println("SUR SAFE SPOT OU PAS ? " + hasMovedToSafeSpot);
+            }
+        }
+    }
+
+    /**
+     * Checks whether there is any safe spot around the player.
+     * A safe spot is defined as two consecutive walkable tiles (in a given pattern).
+     */
+    public boolean hasSafeSpot() {
+        int baseX = getMapX();
+        int baseY = getMapY();
+        LevelMap map = getMap();
+
+        for (int[] dir : SAFE_DIRECTIONS) {
+            int dx1 = dir[0], dy1 = dir[1];
+            int dx2 = dir[2], dy2 = dir[3];
+
+            int x1 = baseX + dx1, y1 = baseY + dy1;
+            int x2 = baseX + dx2, y2 = baseY + dy2;
+
+            if (isWalkable(x1, y1) && isWalkable(x2, y2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Tries to move the player toward the first valid safe spot found.
+     * The movement is split into two steps based on the SAFE_DIRECTIONS pattern.
+     * Returns true if a safe spot is found and the player is moved.
+     */
+    public boolean moveToSafeSpot() {
+        int baseX = getMapX();
+        int baseY = getMapY();
+
+        for (int[] dir : SAFE_DIRECTIONS) {
+            int dx1 = dir[0], dy1 = dir[1];
+            int dx2 = dir[2], dy2 = dir[3];
+
+            int x1 = baseX + dx1, y1 = baseY + dy1;
+            int x2 = baseX + dx2, y2 = baseY + dy2;
+
+            if (isWalkable(x1, y1) && isWalkable(x2, y2)) {
+                moveBy(dx1, dy1);                      // Move to the first step
+                moveBy(dx2 - dx1, dy2 - dy1);          // Then to the final safe spot
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Moves the player by a relative offset.
+     * @param dx horizontal offset (-1 = left, 1 = right)
+     * @param dy vertical offset (-1 = down, 1 = up)
+     */
+    private void moveBy(int dx, int dy) {
+        if (dx == 1) moveRight();
+        else if (dx == -1) moveLeft();
+
+        if (dy == 1) moveUp();
+        else if (dy == -1) moveDown();
+    }
+
+    /**
+     * Checks if the specified map coordinates are walkable (and in bounds).
+     */
+    private boolean isWalkable(int mapX, int mapY) {
+        LevelMap map = getMap();
+        if (mapX < 0 || mapY < 0 || mapX >= map.getWidth() || mapY >= map.getHeight()) {
+            return false;
+        }
+        return map.getSquare(mapX, mapY).isWalkable();
     }
 
     /**
@@ -95,7 +199,7 @@ public class AIPlayer extends Player {
          * Moves the AI player randomly.
          * This method is called when no players are found or when the AI cannot move towards a player.
          */
-        private void moveRandomly() {
+    private void moveRandomly() {
             int initialX = this.getMapX();
             int initialY = this.getMapY();
             Runnable[] moves = new Runnable[] {
@@ -108,10 +212,12 @@ public class AIPlayer extends Player {
             for (Runnable move : moves) {
                 move.run();
                 if (this.getMapX() != initialX || this.getMapY() != initialY) {
-                    break; 
+                    break;
                 }
             }
         }
+
+
 
 
 }
--- core/src/test/java/com/bomber7/core/AIPlayerTest.java
+++ core/src/test/java/com/bomber7/core/AIPlayerTest.java
@@ -4,6 +4,7 @@ import com.bomber7.core.model.entities.AIPlayer;
 import com.bomber7.core.model.entities.Player;
 import com.bomber7.core.model.map.LevelMap;
 import com.bomber7.core.model.map.LevelMapFactory;
+import com.bomber7.core.model.square.TimeBomb;
 import com.bomber7.utils.GameCharacter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,7 +28,7 @@ class AIPlayerTest {
         levelMap = LevelMapFactory.createLevelMap("foy", 800, 600);
         aiPlayer = new AIPlayer(levelMap, 1, 1, gameCharacter);
         players = new Player[] {
-            new Player("Player2", levelMap, 0, 34, 3, 1, gameCharacter) {}, 
+            new Player("Player2", levelMap, 0, 34, 3, 1, gameCharacter) {},
             new Player("Player1", levelMap, 0, 24, 3, 1, gameCharacter) {}
         };
     }
@@ -78,7 +79,54 @@ class AIPlayerTest {
     void testFindNearestPlayer() {
         Player nearestPlayer = players[1];
         Player foundPlayer = aiPlayer.findNearestPlayer(players);
-        assertEquals(nearestPlayer, foundPlayer, 
+        assertEquals(nearestPlayer, foundPlayer,
         "The nearest player should be Player1 at (0,24), and not Player2 at (0,34)");
     }
+
+    @Test
+    void testMoveTowardsNearestPlayerAndTick() {
+        int initialMapX = aiPlayer.getMapX();
+        assertEquals(1, initialMapX, "Initial map X position should be 1, not " + initialMapX);
+        int initialMapY = aiPlayer.getMapY();
+        assertEquals(1, initialMapY, "Initial map Y position should be 1, not " + initialMapY);
+
+        assertTrue(aiPlayer.getMapX() == 1 && aiPlayer.getMapY() == 1,
+            "Start position should be (1,1) before moving. Actual position: ("
+                + aiPlayer.getMapX() + ", " + aiPlayer.getMapY() + ")");
+
+        System.out.println("Player coord: " + aiPlayer.getMapX() + ", " + aiPlayer.getMapY());
+
+        int xBeforeTryPlacingBomb = aiPlayer.getMapX();
+        int yBeforeTryPlacingBomb = aiPlayer.getMapY();
+
+        assertFalse(aiPlayer.hasSafeSpot());
+        aiPlayer.tick(1.0f); // Simulate a tick with a delta time of 1 seconds
+        System.out.println("Player coord: " + aiPlayer.getMapX() + ", " + aiPlayer.getMapY());
+        assertFalse(levelMap.getSquare(xBeforeTryPlacingBomb, yBeforeTryPlacingBomb).getMapElement() instanceof TimeBomb);
+
+        aiPlayer.moveAI(players);
+
+        assertTrue(aiPlayer.hasSafeSpot());
+
+        System.out.println("Player coord: " + aiPlayer.getMapX() + ", " + aiPlayer.getMapY());
+
+
+        assertTrue(aiPlayer.getMapX() == 1 && aiPlayer.getMapY() == 2,
+            "AI should move towards Player1 at (0,23). AI player should be at (1,2)."
+                + " Actual position: (" + aiPlayer.getMapX() + ", " + aiPlayer.getMapY() + ")");
+
+        xBeforeTryPlacingBomb = aiPlayer.getMapX();
+        yBeforeTryPlacingBomb = aiPlayer.getMapY();
+
+        aiPlayer.tick(1.0f); // Simulate a tick with a delta time of 1 seconds
+        System.out.println("Player coord: " + aiPlayer.getMapX() + ", " + aiPlayer.getMapY());
+
+        assertTrue(levelMap.getSquare(xBeforeTryPlacingBomb, yBeforeTryPlacingBomb).getMapElement() instanceof TimeBomb);
+
+        assertEquals(2, aiPlayer.getMapX());
+        assertEquals(1, aiPlayer.getMapY());
+
+        System.out.println("Player coord: " + aiPlayer.getMapX() + ", " + aiPlayer.getMapY());
+    }
+
 }
